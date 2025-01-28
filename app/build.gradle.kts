27b6638b64538d149e54658c33208432c2739712
Daniel <kuanshenwang@gmail.com>
2025-01-28 20:54:27 +0800

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Custom
+    implementation(libs.androidx.compose.material)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson)
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+    implementation(libs.coil.compose)
+
 }
