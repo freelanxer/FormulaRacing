41f41c81fbe2caf7e0e988b6b8c4fc99cfb74621
Daniel <kuanshenwang@gmail.com>
2025-01-30 23:17:18 +0800

    Add style resource

---
+++ app/src/main/java/com/freelanxer/formularacing/ui/theme/RacingTextStyle.kt
@@ -0,0 +1,20 @@
+package com.freelanxer.formularacing.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val RacingTextTitle = TextStyle(
+    fontSize = 18.sp,
+    fontWeight = FontWeight.Bold
+)
+
+val RacingTextSecondTitle = TextStyle(
+    fontSize = 14.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val RacingTextContentTitle = TextStyle(
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Bold
+)
